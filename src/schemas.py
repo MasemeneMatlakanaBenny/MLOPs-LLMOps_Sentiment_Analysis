27d1aeddeefd724a52fc748843b67f14ec588957
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2025-11-11 06:34:26 +0200

    added content to the schemas file

--- src/schemas.py
+++ src/schemas.py
@@ -1,1 +1,15 @@
+from pydantic import BaseModel,field_validator
+
+
+class Prompt(BaseModel):
+    message:str
+
+    @field_validator("message")
+    @classmethod
+    def validate_message(value,cls):
+        if type(value)!=str:
+            raise TypeError("Invalid Data Type")
+        
+        return value
+    
 
