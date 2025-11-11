fc05c099341cd13864c56989ba59efb46cd02519
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2025-11-11 07:11:31 +0200

    productionizing the model

---
+++ lifecycle/production.py
@@ -0,0 +1,28 @@
+import mlflow
+from src.mlflow_configurations import set_mlflow_exp,set_mlflow_host,mlflow_client,load_model_name
+
+
+
+## set the experiment name within the workflow first:
+set_mlflow_exp()
+
+## set the mlflow local host too
+set_mlflow_host()
+
+
+## create the mlflow client first:
+client=mlflow_client()
+
+
+## create the variables using the imported functions:
+model_name=load_model_name()
+model_version="1"
+
+
+# stage the model using the client variable created earlier in the workflow:
+client.transition_model_version_stage(
+    name=model_name,
+    version=model_version,
+    stage="production"
+)
+
